a859108e82af1256fc41f5c468e57e3a3bf291e4
Amanda E Nelson <amanda.nelson@yahoo.com>
2019-06-15 15:42:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,14 +29,15 @@ def move(board, index, char = "X")
 end
 
 def turn(board)
+  loop do
   puts "Please enter 1-9:"
   input = gets.chomp
   index = input_to_index(input)
-  if valid_move?(board, index) == false
-    puts "invalid"
-    input = gets.chomp
-  else
-    board[index] = "X"
-    display_board(board)
+  if valid_move?(board, index) == true
+  board[index] = "X"
+  display_board(board)
+    break
+  end
+puts "Invalid selection"
   end
 end
